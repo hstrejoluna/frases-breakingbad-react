58bfc8a95797fee746c09efa1ba6f20d1504de26
Héctor Sebastián Trejo Luna <32781378+hstrejoluna@users.noreply.github.com>
2019-08-12 15:43:56 -0500

    Add files via upload

---
+++ build/precache-manifest.a5762dbda1c0a223d4538798ab629036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3ce09704af613e718239e0584c3818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a54a6aab7517868cdf",
+    "url": "/static/css/main.6f2a6c83.chunk.css"
+  },
+  {
+    "revision": "7c710bf91d7cc0ec053e",
+    "url": "/static/js/2.deb9afcc.chunk.js"
+  },
+  {
+    "revision": "a4a54a6aab7517868cdf",
+    "url": "/static/js/main.5b75e8c1.chunk.js"
+  },
+  {
+    "revision": "3d5dfa5d1f647d8521b7",
+    "url": "/static/js/runtime~main.3d352c2d.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
